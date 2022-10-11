ac2d2cc22ec41932e50b2e7f6ae5222db46c34a1
Anthony Harris <vibe@ah.id.au>
2022-10-12 00:13:46 +0100

    Move TernaryResult type into the module
    and flatten the module.

--- TernaryResult.fs
+++ TernaryResult.fs
@@ -1,57 +1,54 @@
-﻿namespace Combinators
-
-open FSharpx.Collections
-
-module L = LazyList
+﻿module Combinators.TernaryResult
 
 type TernaryResult<'a, 'Error> =
     | Ok of 'a
     | Ignore
     | Error of 'Error
 
-module TernaryResult =
-    open Standard
+open FSharpx.Collections
+open Combinators.Standard
+module L = LazyList
 
-    let inline bind2 v f c =
-        match c with
-        | Ok x -> f x
-        | Ignore -> v
-        | Error r -> Error r
+let inline bind2 v f c =
+    match c with
+    | Ok x -> f x
+    | Ignore -> v
+    | Error r -> Error r
 
-    let inline bind f = bind2 Ignore f
+let inline bind f = bind2 Ignore f
 
-    let inline map f = bind (f >> Ok)
+let inline map f = bind (f >> Ok)
 
-    let ofResult r =
-        match r with
-        | Result.Ok x -> Ok x
-        | Result.Error y -> Error y
+let ofResult r =
+    match r with
+    | Result.Ok x -> Ok x
+    | Result.Error y -> Error y
 
-    let toResult resultOfIgnore tr =
-        match tr with
-        | Ok x -> Result.Ok x
-        | Ignore -> resultOfIgnore
-        | Error msg -> Result.Error msg
+let toResult resultOfIgnore tr =
+    match tr with
+    | Ok x -> Result.Ok x
+    | Ignore -> resultOfIgnore
+    | Error msg -> Result.Error msg
 
-    let isOk tr =
-        match tr with
-        | Ok _ -> true
-        | _ -> false
+let isOk tr =
+    match tr with
+    | Ok _ -> true
+    | _ -> false
 
-    let shouldIgnore tr =
-        match tr with
-        | Ignore _ -> true
-        | _ -> false
+let shouldIgnore tr =
+    match tr with
+    | Ignore _ -> true
+    | _ -> false
 
-    let SS f g h x = f (g x) (h x)
+let SS f g h x = f (g x) (h x)
 
-    let groupResult xs: TernaryResult<LazyList<'a>, 'b> =
-        let folder acc =
-            let accBind =
-                SS bind2
-                    (Seq.singleton >> L.ofSeq >> Ok)
-                    (Seq.singleton >> L.ofSeq >> C L.append >> B Ok)
+let groupResult xs: TernaryResult<LazyList<'a>, 'b> =
+    let folder acc =
+        let accBind =
+            SS bind2
+                (Seq.singleton >> L.ofSeq >> Ok)
+                (Seq.singleton >> L.ofSeq >> C L.append >> B Ok)
 
-            S bind2 (C accBind) acc
+        S bind2 (C accBind) acc
 
-        L.fold folder Ignore xs
+    L.fold folder Ignore xs
