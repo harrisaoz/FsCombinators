b8cde97a30c7f2050aa847ac44a11df451dac67f
Anthony Harris <vibe@ah.id.au>
2022-11-22 11:58:47 +0000

    New feature: facilitate simple handling of exceptions as a Result.

---
+++ FsCombinators/ResultExtensions.fs
@@ -0,0 +1,15 @@
+﻿module FsCombinators.ResultExtensions
+
+/// Execute f in a try block,
+/// returning its result wrapped in Result.Ok if no exception is raised,
+/// or the exception message wrapped in Result.Error instead of raising the
+/// exception.
+///
+/// Note that OutOfMemoryException is re-raised in order to avoid
+/// non-deterministic program behaviour.
+let tryAsResult f =
+    try
+        f () |> Result.Ok
+    with
+    | :? System.OutOfMemoryException -> reraise ()
+    | ex -> Result.Error ex.Message
