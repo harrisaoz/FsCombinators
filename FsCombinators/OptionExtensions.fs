a66e56849cc2b706eacbe66a30be31a02e8dd445
Anthony Harris <vibe@ah.id.au>
2022-11-22 11:58:30 +0000

    New feature: facilitate simple handling of exceptions as an Option value.

---
+++ FsCombinators/OptionExtensions.fs
@@ -0,0 +1,14 @@
+﻿module FsCombinators.OptionExtensions
+
+/// Execute f in a try block,
+/// returning its result wrapped in Option.Some if no exception is raised,
+/// or Option.None instead of raising an exception.
+///
+/// Note that OutOfMemoryException is re-raised in order to avoid
+/// non-deterministic program behaviour.
+let tryAsOption f =
+    try
+        f () |> Option.Some
+    with
+    | :? System.OutOfMemoryException -> reraise ()
+    | _ -> None
