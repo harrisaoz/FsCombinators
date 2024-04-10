bcea5f955d75660e423a42ecca8361564ff2f31e
Anthony Harris <anthony@ah.id.au>
2024-04-10 12:49:58 +0100

    Fix endsWith and contains parameter ordering. The needle should be the first argument.
    Also:
    - apply standard formatting;
    - document most functions.

--- FsCombinators/StringExtensions.fs
+++ FsCombinators/StringExtensions.fs
@@ -1,28 +1,61 @@
 ﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
 module FsCombinators.StringExtensions
 
-let contains: string -> string -> bool =
-    fun haystack -> haystack.Contains
+let inline toLower (text: string) = text.ToLower()
 
-let containsCI: string -> string -> bool =
-    fun haystack needle ->
-        haystack.ToLower().Contains(needle.ToLower())
+let inline contains (needle: string) (haystack: string) : bool = haystack.Contains(needle)
 
-let endsWith: string -> string -> bool =
-    fun haystack -> haystack.EndsWith
+/// <summary>
+/// Facilitate case-insensitive comparisons using an arbitrary search function.
+/// </summary>
+/// <param name="comparator">A function to be used to search haystack for a
+/// needle after they have been converted to lowercase.</param>
+/// <param name="needle">The text to find.</param>
+/// <param name="haystack">The text to search.</param>
+let ciComparison comparator needle haystack : bool =
+    comparator (toLower needle) (toLower haystack)
 
-let endsWithCI: string -> string -> bool =
-    fun haystack needle -> haystack.ToLower().EndsWith(needle.ToLower())
+/// A case-insensitive version of <code>contains</code>.
+let containsCI (needle: string) (haystack: string) : bool = ciComparison contains needle haystack
 
-let split (separators: char[]) (whole: string) =
-    whole.Split(separators)
+/// <summary>
+/// Haystack ends with needle, matching case.
+/// </summary>
+/// <param name="needle">The string to look for at the end of haystack.</param>
+/// <param name="haystack">The string to search for the needle in.</param>
+/// <returns>
+/// True if the string <code>haystack</code> ends in the value of <code>needle</code>.
+/// </returns>
+let inline endsWith needle (haystack: string) : bool = haystack.EndsWith(needle)
 
+/// A case-insensitive version of <code>endsWith</code>.
+let endsWithCI (needle: string) (haystack: string) : bool = ciComparison endsWith needle haystack
+
+/// <summary>
+/// Split <code>whole</code> text at each encounter with any character in
+/// <code>separators</code>.
+/// </summary>
+/// <param name="separators">A set of characters to split the text on, none of which
+/// should appear in the returned array.</param>
+/// <param name="whole">The input text to split.</param>
+/// <returns>
+/// An array of segments of the original <code>whole</code> text, without the
+/// separator characters, <code>separators</code>.
+/// </returns>
+let split (separators: char[]) (whole: string) = whole.Split(separators)
+
+/// <summary>
+/// Replace any and all occurrences of the character <code>labelSeparator</code>
+/// in some input text with the <code>replacement</code> text.
+/// </summary>
+/// <param name="labelSeparator">The character to replace.</param>
+/// <param name="replacement">The replacement string for occurrences of
+/// <code>labelSeparator</code>.</param>
 let flatten labelSeparator (replacement: string) =
-    split (Array.singleton labelSeparator)
-    >> String.concat replacement
+    split (Array.singleton labelSeparator) >> String.concat replacement
 
 let iequal (s1: string) (s2: string) =
-    System.String.Equals (s1, s2, System.StringComparison.InvariantCultureIgnoreCase)
+    System.String.Equals(s1, s2, System.StringComparison.InvariantCultureIgnoreCase)
 
 let appendString (toAppend: string) (baseText: string) =
     System.Text.StringBuilder(baseText).Append(toAppend).ToString()
